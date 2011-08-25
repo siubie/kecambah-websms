b3318cdfe50497d4c8ab45366b5c5c4d4e921318
Wahyu Kecambah Linux <wahyu.kelix@gmail.com>
2011-08-25 15:56:17 +0700

    sore commit

--- application/views/inbox_view.php
+++ application/views/inbox_view.php
@@ -1,4 +1,4 @@
 <h2>INBOX</h2>
 <div id="gridContainer">
-	<div id="grid" dojoType="dojox.grid.EnhancedGrid" store="jsonInbox" structure="layout" plugins="plugins"></div>
+	<div id="gridInbox" dojoType="dojox.grid.EnhancedGrid" store="jsonInbox" structure="layout" plugins="plugins"></div>
 </div>
